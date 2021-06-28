8556b8ec79e15b0a5debcc8be7b5cada25d728d9
William Amorim <william.amorim@stone.com.br>
2021-06-28 14:36:36 -0300

    Adding customer mapper

---
+++ src/CqrsExploration.Domain/Mapper/CustomerMapper.cs
@@ -0,0 +1,18 @@
+﻿using CqrsExploration.Domain.Commands.Requests;
+using CqrsExploration.Domain.Commands.Responses;
+using CqrsExploration.Domain.Entities;
+
+namespace CqrsExploration.Domain.Mapper
+{
+    public static class CustomerMapper
+    {
+        public static CreateCustomerResponse MapToResponse(Customer customer) => new()
+        {
+            CreatedAt = customer.CreatedAt,
+            FullName = customer.ToString(),
+            Id = customer.Id
+        };
+
+        public static Customer MapToEntity(CreateCustomerRequest request) => new(request.FirstName, request.LastName, request.Email);
+    }
+}
