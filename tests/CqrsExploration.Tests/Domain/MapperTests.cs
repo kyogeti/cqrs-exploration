6604b1be8fd0002c00af6aa76c4c30ccc4dbf11d
William Amorim <william.amorim@stone.com.br>
2021-06-28 14:37:11 -0300

    Adding tests

---
+++ tests/CqrsExploration.Tests/Domain/MapperTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+using CqrsExploration.Domain.Commands.Requests;
+using CqrsExploration.Domain.Entities;
+using CqrsExploration.Domain.Mapper;
+using Xunit;
+
+namespace CqrsExploration.Tests.Domain
+{
+    public class MapperTests
+    {
+        [Fact(DisplayName = "Map to response correctly")]
+        public void CustomerMapper_OnMapToResponse_MapCorrectly()
+        {
+            //Arrange
+            var customer = new Customer("customer", "name", "customer@customer.com");
+
+            //Act
+            var result = CustomerMapper.MapToResponse(customer);
+
+            //Assert
+            Assert.Equal($"{customer.FirstName} {customer.LastName} ({customer.Email})", result.FullName);
+        }
+
+        [Fact(DisplayName = "Map to entity correctly")]
+        public void CustomerMapper_OnMapToEntity_MapCorrectly()
+        {
+            // Arrange
+            var request = new CreateCustomerRequest();
+            request.FirstName = "customer";
+            request.LastName = "name";
+            request.Email = "customer@customer.com";
+
+            // Act
+            var result = CustomerMapper.MapToEntity(request);
+
+            // Assert
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+        }
+    }
+}
