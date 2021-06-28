457be8599b010f4b1625f9d639544cbfa3f8e568
William Amorim <william.amorim@stone.com.br>
2021-06-28 14:36:49 -0300

    Adding validators

---
+++ src/CqrsExploration.Domain/Validations/CreateCustomerRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using CqrsExploration.Domain.Commands.Requests;
+using FluentValidation;
+
+namespace CqrsExploration.Domain.Validations
+{
+    public class CreateCustomerRequestValidator : AbstractValidator<CreateCustomerRequest>
+    {
+        public CreateCustomerRequestValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress();
+
+        }
+    }
+}
---
+++ src/CqrsExploration.Domain/Validations/CustomerValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CqrsExploration.Domain.Entities;
+using FluentValidation;
+
+namespace CqrsExploration.Domain.Validations
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+
+            RuleFor(x => x.Id)
+                .NotNull()
+                .NotEqual(Guid.Empty);
+
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.CreatedAt)
+                .Must(x => x > DateTime.MinValue);
+
+        }
+    }
+}
