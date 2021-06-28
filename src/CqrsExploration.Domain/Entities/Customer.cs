c0ef142790e1742b1663a7b084939309e2e74220
William Amorim <william.amorim@stone.com.br>
2021-06-28 14:36:25 -0300

    Creating customer entity

---
+++ src/CqrsExploration.Domain/Entities/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CqrsExploration.Domain.Entities
+{
+    public class Customer
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        [NotNull, Required]
+        [StringLength(1, ErrorMessage = "First Name must have at least one character.")]
+        [MaxLength(50, ErrorMessage = "First Name must have less than 50 characters.")]
+        public string FirstName { get; private set; }
+
+        [NotNull, Required]
+        [StringLength(1, ErrorMessage = "Last Name must have at least one character.")]
+        [MaxLength(50, ErrorMessage = "Last Name must have less than 50 characters.")]
+        public string LastName { get; private set; }
+
+        [NotNull, Required]
+        [EmailAddress(ErrorMessage = "Invalid e-mail address.")]
+        public string Email { get; private set; }
+
+        [NotNull, Required]
+        public DateTime CreatedAt { get; private set; }
+
+        public Customer(string firstName, string lastName, string email)
+        {
+
+            if (string.IsNullOrEmpty(firstName) ||
+                string.IsNullOrEmpty(lastName) ||
+                string.IsNullOrEmpty(email))
+                throw new ArgumentException("firstName, lastName and email are required.");
+
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            CreatedAt = DateTime.UtcNow;
+
+        }
+
+        public override string ToString() => $"{FirstName} {LastName} ({Email})";
+    }
+}
